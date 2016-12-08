4033eb69a6949a614c5400ffd8bdc1ab801b23b3
Benson <benson@Bensonde-iMac.local>
2016-12-08 11:57:07 +0800

    [change get user way]

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var CLASS = process.env.LOG_SYSTEM_CLASS;
 
 
 
-var responseCB = function(err, res, body, cb){
+var responseCB = function (err, res, body, cb) {
   if (err) {
     if (cb) cb(err, null);
     console.error(err);
@@ -22,49 +22,51 @@ var responseCB = function(err, res, body, cb){
 };
 
 var add = function (_payload, cb) {
-        var url = BASE_URL + '/' + CLASS;
+  var url = BASE_URL + '/' + CLASS;
 
-        var headers = {
-        'X-LC-Id': process.env.LOG_SYSTEM_APP_ID,
-        'X-LC-Key': process.env.LOG_SYSTEM_APP_KEY,
-        'Content-Type': 'application/json'
-        };
-        
-        var options = {
-            uri: url,
-            method: 'POST',
-            headers: headers,
-            json: _payload
-        };
+  var headers = {
+    'X-LC-Id': process.env.LOG_SYSTEM_APP_ID,
+    'X-LC-Key': process.env.LOG_SYSTEM_APP_KEY,
+    'Content-Type': 'application/json'
+  };
 
-        request(options, function (err, res, body) {
-            responseCB(err, res, body, cb)
-        });
+  var options = {
+    uri: url,
+    method: 'POST',
+    headers: headers,
+    json: _payload
+  };
+
+  request(options, function (err, res, body) {
+    responseCB(err, res, body, cb)
+  });
 }
 
 var remove = function (_objectId, cb) {
-        var url = BASE_URL + '/' + CLASS+ '/' +_objectId;
+  var url = BASE_URL + '/' + CLASS + '/' + _objectId;
 
-        var headers = {
-        'X-LC-Id': process.env.LOG_SYSTEM_APP_ID,
-        'X-LC-Key': process.env.LOG_SYSTEM_APP_KEY,
-        'Content-Type': 'application/json'
-        };
-        
-        var options = {
-        uri: url,
-        method: 'DELETE',
-        headers: headers
-        };
+  var headers = {
+    'X-LC-Id': process.env.LOG_SYSTEM_APP_ID,
+    'X-LC-Key': process.env.LOG_SYSTEM_APP_KEY,
+    'Content-Type': 'application/json'
+  };
 
-        request(options, function (err, res, body) {
-            responseCB(err, res, body, cb)
-        });
+  var options = {
+    uri: url,
+    method: 'DELETE',
+    headers: headers
+  };
+
+  request(options, function (err, res, body) {
+    responseCB(err, res, body, cb)
+  });
 }
 
 var Request = function (req, res, next) {
-
-    var payload = {
+  var token = req.cookies.sessiontoken || req.headers.sessiontoken;
+  if (token) {
+    AV.User.become(token).then(function (user) {
+      var payload = {
         protocol: req.protocol || '',
         host: req.get('host') || '',
         originalUrl: req.originalUrl || '',
@@ -75,45 +77,103 @@ var Request = function (req, res, next) {
         platform: req.headers ? req.headers.platform : '',
         platformVersion: req.headers ? req.headers.platformVersion : '',
         brand: req.headers ? req.headers.brand : '',
-        xRealIp: req.headers ? req.headers['x-real-ip']: '',
-        xForwardedFor: req.headers ? req.headers['x-forwarded-for']: '',
-        userId: AV.User.current()? AV.User.current().id : '',
-        username: AV.User.current()? AV.User.current().get('username') : '',
-        reqOrRes: 'Request'
+        xRealIp: req.headers ? req.headers['x-real-ip'] : '',
+        xForwardedFor: req.headers ? req.headers['x-forwarded-for'] : '',
+        userId: AV.User.current() ? AV.User.current().id : '',
+        username: AV.User.current() ? AV.User.current().get('username') : '',
+        reqOrRes: 'Request',
+        userId_new: user ? user.id : '',
+        username_new: user ? user.get('username') : '',
+      };
+      add(payload);
+      next();
+    });
+  } else {
+    var payload = {
+      protocol: req.protocol || '',
+      host: req.get('host') || '',
+      originalUrl: req.originalUrl || '',
+      body: req.body ? JSON.stringify(req.body) : '',
+      cookies: req.cookies ? JSON.stringify(req.cookies) : '',
+      header: req.headers ? JSON.stringify(req.headers) : '',
+      version: req.headers ? req.headers.version : '',
+      platform: req.headers ? req.headers.platform : '',
+      platformVersion: req.headers ? req.headers.platformVersion : '',
+      brand: req.headers ? req.headers.brand : '',
+      xRealIp: req.headers ? req.headers['x-real-ip'] : '',
+      xForwardedFor: req.headers ? req.headers['x-forwarded-for'] : '',
+      userId: AV.User.current() ? AV.User.current().id : '',
+      username: AV.User.current() ? AV.User.current().get('username') : '',
+      reqOrRes: 'Request'
     };
-    add(payload)
-    next()
-}
+    add(payload);
+    next();
+  }
+
+};
 
 var Response = function (req, res, next) {
-    var ORIGIN_JSON_FUNCTION = res.json
-
-    res.json = function (_result) {
+  var ORIGIN_JSON_FUNCTION = res.json;
+  var token = req.cookies.sessiontoken || req.headers.sessiontoken;
+  if (token) {
+    AV.User.become(token).then(function (user) {
+      res.json = function (_result) {
 
         var payload = {
-            protocol: req.protocol || '',
-            host: req.get('host') || '',
-            originalUrl: req.originalUrl || '',
-            body: req.body ? JSON.stringify(req.body) : '',
-            header: req.headers ? JSON.stringify(req.headers) : '',
-            version: req.headers ? req.headers.version : '',
-            platform: req.headers ? req.headers.platform : '',
-            platformVersion: req.headers ? req.headers.platformVersion : '',
-            brand: req.headers ? req.headers.brand : '',
-            device: req.headers ? req.headers.device : '',
-            xRealIp: req.headers ? req.headers['x-real-ip']: '',
-            xForwardedFor: req.headers ? req.headers['x-forwarded-for']: '',
-            userId: AV.User.current()? AV.User.current().id : '',
-            username: AV.User.current()? AV.User.current().get('username') : '',
-            response: JSON.stringify(_result) || '',
-            reqOrRes: 'Response'
+          protocol: req.protocol || '',
+          host: req.get('host') || '',
+          originalUrl: req.originalUrl || '',
+          body: req.body ? JSON.stringify(req.body) : '',
+          header: req.headers ? JSON.stringify(req.headers) : '',
+          version: req.headers ? req.headers.version : '',
+          platform: req.headers ? req.headers.platform : '',
+          platformVersion: req.headers ? req.headers.platformVersion : '',
+          brand: req.headers ? req.headers.brand : '',
+          device: req.headers ? req.headers.device : '',
+          xRealIp: req.headers ? req.headers['x-real-ip'] : '',
+          xForwardedFor: req.headers ? req.headers['x-forwarded-for'] : '',
+          userId: AV.User.current() ? AV.User.current().id : '',
+          username: AV.User.current() ? AV.User.current().get('username') : '',
+          response: JSON.stringify(_result) || '',
+          reqOrRes: 'Response',
+          userId_new: user ? user.id : '',
+          username_new: user ? user.get('username') : '',
         };
 
-        add(payload)
-        ORIGIN_JSON_FUNCTION.call(this, _result)
-    }
+        add(payload);
+        ORIGIN_JSON_FUNCTION.call(this, _result);
+      };
+      next();
+    });
+
+  } else {
+    res.json = function (_result) {
+
+      var payload = {
+        protocol: req.protocol || '',
+        host: req.get('host') || '',
+        originalUrl: req.originalUrl || '',
+        body: req.body ? JSON.stringify(req.body) : '',
+        header: req.headers ? JSON.stringify(req.headers) : '',
+        version: req.headers ? req.headers.version : '',
+        platform: req.headers ? req.headers.platform : '',
+        platformVersion: req.headers ? req.headers.platformVersion : '',
+        brand: req.headers ? req.headers.brand : '',
+        device: req.headers ? req.headers.device : '',
+        xRealIp: req.headers ? req.headers['x-real-ip'] : '',
+        xForwardedFor: req.headers ? req.headers['x-forwarded-for'] : '',
+        userId: AV.User.current() ? AV.User.current().id : '',
+        username: AV.User.current() ? AV.User.current().get('username') : '',
+        response: JSON.stringify(_result) || '',
+        reqOrRes: 'Response'
+      };
+
+      add(payload);
+      ORIGIN_JSON_FUNCTION.call(this, _result);
+    };
     next();
-}
+  }
+};
 
 
 // adding log to logSystem through RESTful API
